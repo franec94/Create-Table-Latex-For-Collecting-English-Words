6888020d7c319c32a200bc66e2448c79912e6d59
franec94 <franec94@gmail.com>
2020-04-30 13:04:16 +0000

    Well designed list of tables as within content of output file

--- utils/business/table_funcs.py
+++ utils/business/table_funcs.py
@@ -18,16 +18,33 @@ import yaml
 
 def write_tables_2_file(tables_str: list, header_words: list, step: int, output_file: str = 'out.txt') -> None:
   """Write to default out.txt file tables listed separated by means of two blank lines"""
-  for ii, table in enumerate(tables_str):
-    with open('out.txt'.format(str(ii)), "a") as f:
-      print('write table on file...')
+  
+  with open('out.txt', "w") as f:
+    for ii, table in enumerate(tables_str):
+      # print('write table on file...')
+      
+      
       f.write('\n')
       f.write('\n')
+
+      f.write('\\begin{center}')
+      f.write('\\begin{tabular}{ |' + 'c|' * step + '}')
+      f.write('\\hline\n')
+      
+      f.write('\\multicolumn{%d}{|c|}{\\textbf{Synonims - Tab no.??}} \\\\' % (step, ))
+      f.write('\\hline\n')
+      # Header
       hw = header_words[ii*step:(ii+1)*step]
       res = ' & '.join([xi for xi in hw]) 
       f.write(res + ' \\\\\n')
+
+      # Content
       f.write('\\hline\n')
       f.writelines(table)
+      f.write('\\hline\n')
+
+      f.write('\\end{tabular}')
+      f.write('\\end{center}')
       pass
     pass
   pass
